e06cee06869f3d0dc3dbd862d2453c089388e07a
sjurate <rasaujuratei@gmail.com>
2023-04-13 10:18:03 +0300

    Added material UI style for form + small css fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { LandingComponent } from './landing/landing.component';
-import { MatButtonModule } from '@angular/material/button';
 import { LoanFormComponent } from './loan-form/loan-form.component';
-import {ReactiveFormsModule} from "@angular/forms";
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -18,13 +20,15 @@ import {ReactiveFormsModule} from "@angular/forms";
     LandingComponent,
     LoanFormComponent,
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        MatButtonModule,
-        ReactiveFormsModule,
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/loan-form/loan-form.component.ts
+++ src/app/loan-form/loan-form.component.ts
@@ -1,52 +1,73 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
-import {Subject} from "rxjs";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Subject } from 'rxjs';
+import { FloatLabelType } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-loan-form',
   templateUrl: './loan-form.component.html',
-  styleUrls: ['./loan-form.component.scss']
+  styleUrls: ['./loan-form.component.scss'],
 })
-export class LoanFormComponent implements OnInit, OnDestroy{
+export class LoanFormComponent implements OnInit, OnDestroy {
   private destroy$ = new Subject<void>();
   loanForm: FormGroup;
   constructor(private fb: FormBuilder) {
-
     this.loanForm = this.fb.group({
       totalAmount: ['', [Validators.required, Validators.min(1000)]],
       downPayment: ['', [Validators.required]],
-      termYears: ['', [Validators.required, Validators.min(1), Validators.max(30)], ]
+      termYears: [
+        '',
+        [Validators.required, Validators.min(1), Validators.max(30)],
+      ],
     });
-    this.loanForm.get('totalAmount')!.valueChanges.subscribe(totalAmount => {
+
+    this.loanForm.get('totalAmount')!.valueChanges.subscribe((totalAmount) => {
       const downPaymentControl = this.loanForm.get('downPayment');
       if (totalAmount >= 1000) {
         if (!downPaymentControl?.value || downPaymentControl?.pristine) {
           const downPayment = totalAmount * 0.15;
           downPaymentControl?.setValue(downPayment);
         }
-        downPaymentControl?.setValidators([Validators.required, Validators.min(totalAmount * 0.15), Validators.max(totalAmount * 0.90)]);
+        downPaymentControl?.setValidators([
+          Validators.required,
+          Validators.min(totalAmount * 0.15),
+          Validators.max(totalAmount * 0.9),
+        ]);
       } else {
         downPaymentControl?.reset();
       }
     });
   }
 
-  ngOnInit(): void {
-
-    }
+  ngOnInit(): void {}
   ngOnDestroy(): void {
     this.destroy$.next();
   }
-  get totalAmount(){
-    return this.loanForm.get('totalAmount') as FormControl<number>
+  get totalAmount() {
+    return this.loanForm.get('totalAmount') as FormControl<number>;
   }
 
-  get downPayment(){
-    return this.loanForm.get('downPayment') as FormControl<number>
+  get downPayment() {
+    return this.loanForm.get('downPayment') as FormControl<number>;
   }
 
-  get termYears(){
-    return this.loanForm.get('termYears') as FormControl<number>
+  get termYears() {
+    return this.loanForm.get('termYears') as FormControl<number>;
+  }
+
+  getTotalAmountFloatLabelValue(): FloatLabelType {
+    return this.loanForm.get('totalAmount')!.value || 'auto';
+  }
+  getDownPaymentFloatLabelValue(): FloatLabelType {
+    return this.loanForm.get('totalAmount')!.value || 'auto';
+  }
+  getTermYearsFloatLabelValue(): FloatLabelType {
+    return this.loanForm.get('termYears')!.value || 'auto';
   }
 
   onLoanFormSubmit() {
@@ -59,6 +80,4 @@ export class LoanFormComponent implements OnInit, OnDestroy{
   onLoanFormReset() {
     this.loanForm.reset();
   }
-
-
 }
