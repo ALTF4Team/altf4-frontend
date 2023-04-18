efd683328d6b2ee7d7918862edef54ead806df40
sharat789 <sharat157@gmail.com>
2023-04-18 14:59:37 +0300

    removed comments

--- src/app/pie-loan/pie-loan.component.ts
+++ src/app/pie-loan/pie-loan.component.ts
@@ -19,42 +19,6 @@ export class PieLoanComponent implements OnChanges {
   @Input() loanInfo?: MonthlyInterest | null;
   public chart: any;
 
-  // ngOnInit(): void {
-  //   this.loanInfo?.subscribe((loan) => {
-  //     this.chart = new Chart('myChart', {
-  //       type: 'pie',
-  //       data: {
-  //         labels: ['Loan Amount', 'Total Interest Paid'],
-  //         datasets: [
-  //           {
-  //             data: [loan.loanAmount, loan.totalInterestAmount],
-  //             backgroundColor: ['#0883ff', '#f3067c'],
-  //             borderWidth: 1,
-  //             borderColor: '#fff', // Change border color to white
-  //           },
-  //         ],
-  //       },
-  //       options: {
-  //         responsive: true,
-  //         maintainAspectRatio: false,
-  //         plugins: {
-  //           legend: {
-  //             position: 'bottom',
-  //             labels: {
-  //               color: '#ffffff',
-  //             },
-  //           },
-  //           title: {
-  //             display: true,
-  //             text: 'Loan Summary',
-  //             color: '#ffffff',
-  //           },
-  //         },
-  //       },
-  //     });
-  //   });
-  // }
-
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['loanInfo'] && changes['loanInfo'].currentValue) {
       this.chart = new Chart('myChart', {
